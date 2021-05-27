b30c2245550051b9561926121d58a8f81e98b29f
Ernesto Casabella Martinez <ernesqba@gmail.com>
2021-05-27 11:58:55 -0400

    Min changes inside logic of encrypt and decrypt

--- services/jweServices.js
+++ services/jweServices.js
@@ -5,7 +5,7 @@ const keyStore = jose.JWK.createKeyStore();
 const formatKeys = 'pem';
 const textEncode = 'utf8';
 
-const generatePemKeys = async(id, accessCode) => {
+const generatePemKeys = async (id, accessCode) => {
   try {
     const props = {
       kid: id,
@@ -16,19 +16,19 @@ const generatePemKeys = async(id, accessCode) => {
     const key = await keyStore.generate('RSA', 2048, props);
     const publicKey = key.toPEM();
     const privateKey = key.toPEM(true);
-    fs.writeFile('./assets/pemPublic.txt', publicKey, ()=>{});
-    fs.writeFile('./assets/pemPrivate.txt', privateKey, ()=>{});
+    fs.writeFile('./assets/pemPublic.txt', publicKey, () => {});
+    fs.writeFile('./assets/pemPrivate.txt', privateKey, () => {});
     return 'Guardado en assets/';
-  }
+  } 
   catch (error) {
     return { error: true, message: error };
   }
 };
 
-const encrypt = async(key, payload) => {
+const encrypt = async (key, payload) => {
   try {
     const publicKey = await jose.JWK.asKey(key, formatKeys);
-    const dataToEncrypt = JSON.stringify(payload);
+    const dataToEncrypt = JSON.stringify(JSON.stringify(payload));
     const encryptOptions = {
       compact: true,
       fields: {
@@ -36,22 +36,24 @@ const encrypt = async(key, payload) => {
         typ: 'jwe'
       }
     };
-    const token = await jose.JWE.createEncrypt(encryptOptions, publicKey).update(dataToEncrypt, textEncode).final();
+    const token = await jose.JWE.createEncrypt(encryptOptions, publicKey)
+      .update(dataToEncrypt, textEncode)
+      .final();
     return token;
-  }
+  } 
   catch (error) {
     return error;
   }
 };
 
-const decrypt = async(key, token) => {
+const decrypt = async (key, token) => {
   try {
     const privateKey = await jose.JWK.asKey(key, formatKeys);
     const decryptedToken = await jose.JWE.createDecrypt(privateKey).decrypt(token);
     const json = JSON.stringify(decryptedToken.plaintext);
     const bufferOriginal = Buffer.from(JSON.parse(json).data);
-    return JSON.parse(bufferOriginal.toString(textEncode));
-  }
+    return JSON.parse(JSON.parse(bufferOriginal.toString(textEncode)));
+  } 
   catch (error) {
     return error;
   }
